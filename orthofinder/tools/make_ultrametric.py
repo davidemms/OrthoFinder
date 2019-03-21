1ea99cec89e456dbf1b63963a3b155cf319628c0
David Emms <david_emms@hotmail.com>
2019-03-21 11:01:21 +0000

    Add script to convert trees to ultrametric

---
+++ orthofinder/tools/make_ultrametric.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import argparse
+import numpy as np
+
+if __name__ == "__main__" and __package__ is None:
+    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from scripts import tree
+
+def AveDist(node):
+    return np.average([node.get_distance(l) for l in node.get_leaf_names()]) 
+
+def Fail():
+    print("ERROR: An error occurred, please review error messages for more information.")
+    sys.exit(1)
+
+def CheckTree(t):
+    if not 2 == len(t.get_children()):
+        print("Input tree must be rooted")
+        Fail()
+
+def main(tree_fn):
+    if not os.path.exists(tree_fn):
+        print("Input tree file does not exist: %s" % tree_fn)
+    t = tree.Tree(tree_fn, format=1)
+    CheckTree(t)
+    d = AveDist(t)
+    print("Average distance from root to leaves: %f" % d)
+    for n in t.traverse('preorder'):
+        if n.is_root():
+            n.dist = 0
+            continue
+        # work downwards, setting the branch distances from the top down
+        x = t.get_distance(n) - n.dist
+        y = n.dist
+        print("\nTaxa:")
+        print(", ".join(n.get_leaf_names()))
+        if n.is_leaf():
+            z = 0.
+        else:
+            z = AveDist(n)
+        print("Distance of parent node from root: %f" % x)
+        print("Current branch length: %f" % y)
+        print("Average distance to leaves: %f" % z)
+        if (y+z) == 0.:
+            n.dist = 0
+        else:
+            f = (d-x)/(y + z)
+            n.dist = f * n.dist
+        print("Branch length for ultrametric tree: %f" % n.dist)
+    outfn = tree_fn + ".ultrametric.tre"
+    t.write(outfile=outfn, format=5)
+    print("\nUltrametric tree written to: %s\n" % outfn)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("tree_fn")
+    args = parser.parse_args()
+    main(args.tree_fn)
