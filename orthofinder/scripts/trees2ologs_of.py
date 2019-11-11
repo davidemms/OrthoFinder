63dca936ada5a9f4872d1b46e801060e6e9a768a
David Emms <david_emms@hotmail.com>
2019-11-11 12:05:37 +0000

    Fix return statement for missing tree, resolves #298

--- orthofinder/scripts/trees2ologs_of.py
+++ orthofinder/scripts/trees2ologs_of.py
@@ -305,7 +305,7 @@ def CheckAndRootTree(treeFN, species_tree_rooted, GeneToSpecies):
     Root tree
     Returns None if this fails, i.e. checks: exists, has more than one gene, can be rooted
     """
-    if (not os.path.exists(treeFN)) or os.stat(treeFN).st_size == 0: return
+    if (not os.path.exists(treeFN)) or os.stat(treeFN).st_size == 0: return None, False
     qHaveSupport = False
     try:
         tree = tree_lib.Tree(treeFN, format=2)
@@ -315,9 +315,9 @@ def CheckAndRootTree(treeFN, species_tree_rooted, GeneToSpecies):
             tree = tree_lib.Tree(treeFN)
         except:
             tree = tree_lib.Tree(treeFN, format=3)
-    if len(tree) == 1: return 
+    if len(tree) == 1: return None, False
     root = GetRoot(tree, species_tree_rooted, GeneToSpecies)
-    if root == None: return 
+    if root == None: return None, False
     # Pick the first root for now
     if root != tree:
         tree.set_outgroup(root)
