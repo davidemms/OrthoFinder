9a229fa3dd8aba19e8b2d7c4eb5c48ff097cd134
David Emms <david_emms@hotmail.com>
2019-11-27 14:52:58 +0000

    Add ExampleDataset to install package

--- setup.py
+++ setup.py
@@ -75,13 +75,14 @@ setup(
     # have to be included in MANIFEST.in as well.
      package_data={
           'orthofinder': ['config.json'],
+        #   'ExampleDataset': ['ExampleDataset/Mycoplasma_hyopneumoniae.faa', 'ExampleDataset/Mycoplasma_agalactiae.faa', 'ExampleDataset/Mycoplasma_gallisepticum.faa', 'ExampleDataset/Mycoplasma_genitalium.faa']
      },
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[('orthofinder', ['orthofinder/config.json'])],
+    data_files=[('ExampleDataset', ['ExampleDataset/Mycoplasma_hyopneumoniae.faa', 'ExampleDataset/Mycoplasma_agalactiae.faa', 'ExampleDataset/Mycoplasma_gallisepticum.faa', 'ExampleDataset/Mycoplasma_genitalium.faa'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
