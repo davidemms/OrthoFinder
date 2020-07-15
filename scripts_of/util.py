5818ef6b62c710a7d334e4fd451e08448ab04a0f
David Emms <david_emms@hotmail.com>
2020-07-15 15:08:44 +0100

    Update version number

--- scripts_of/util.py
+++ scripts_of/util.py
@@ -50,7 +50,7 @@ Utilities
 SequencesInfo = namedtuple("SequencesInfo", "nSeqs nSpecies speciesToUse seqStartingIndices nSeqsPerSpecies")    # speciesToUse - list of ints
 
 picProtocol = 1
-version = "2.3.12"
+version = "2.3.14"
     
 def PrintNoNewLine(text):
     parallel_task_manager.PrintNoNewLine(text)
